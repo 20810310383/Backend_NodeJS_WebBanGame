0843295a3d90ce56017a3653c9f26d4f1ff80e64
Do Khac Tu <dokhactu1822@gmail.com>
2025-01-07 03:24:22 +0700

    first commit

---
+++ src/models/SanPham.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+const mongoose_delete = require('mongoose-delete');
+
+const SanPham_Schema = new mongoose.Schema(
+    {
+        TenSP: { type: String, required: false },
+        GiaBan: { type: Number, required: false },
+        GiamGiaSP: { type: Number, default: "0" },
+        urlYoutube: { type: String },
+        MoTa: { type: String, default: "Not thing" },
+        SoLuongBan: { type: Number, required: false, default: "1" },
+        ImageSlider: [{ type: String }],    
+        Image: { type: String, required: false },     
+        IdLoaiSP: {ref: "LoaiSP", type: mongoose.SchemaTypes.ObjectId},
+        SoLuongTon: { type: Number, required: false, default: 1 },  
+        isActive: { type: Boolean, default: true},     
+    },
+    { 
+        timestamps: true,   // createAt, updateAt
+    },
+);
+
+// Override all methods
+SanPham_Schema.plugin(mongoose_delete, { overrideMethods: 'all' });
+
+const SanPham = mongoose.model('SanPham', SanPham_Schema);
+
+module.exports = SanPham;
