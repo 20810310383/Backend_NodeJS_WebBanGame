836c72ab39ea9c084b3f3ee370916c96ef39ff82
Do Khac Tu <dokhactu1822@gmail.com>
2025-01-24 01:55:39 +0700

    up server

--- src/server.js
+++ src/server.js
@@ -26,7 +26,9 @@ const allowedOrigins = [
     'http://localhost:3080', 
     'http://localhost:3081', // Local development    
     'https://frontend-react-js-trang-chu-web-ban-game.vercel.app',
-    'https://frontend-react-js-trang-admin-web-ban-game.vercel.app'
+    'https://frontend-react-js-trang-admin-web-ban-game.vercel.app',
+    'https://projectweb.dokhactu.site',
+    'https://admin-projectweb.dokhactu.site'
 ];
 
 app.use(cors({
