cebf1fed8e5877f97cc327c5815e81e14f2a4874
Do Khac Tu <dokhactu1822@gmail.com>
2025-01-28 20:19:34 +0700

    up

--- src/controllers/KhachHang/khachhang.controller.js
+++ src/controllers/KhachHang/khachhang.controller.js
@@ -238,9 +238,9 @@ module.exports = {
                 });
 
                 // const matchContent = sePayWebhookData.content.match(/NAP([a-f0-9]{24})/);
-                const matchContent = sePayWebhookData.content.match(/bomxu([a-zA-Z0-9]{6,24})/);
+                const matchContent = sePayWebhookData.content.match(/NAP([a-zA-Z0-9]{6,24})/);
                 console.log("matchContent: ", matchContent);                
-                const idUser = matchContent[0].replace("bomxu ", "");
+                const idUser = matchContent[0].replace("NAP ", "");
                 console.log("idUser: ", idUser);                
                 const updatedUser = await AccKH.findOneAndUpdate(
                     // { _id: idUser },
