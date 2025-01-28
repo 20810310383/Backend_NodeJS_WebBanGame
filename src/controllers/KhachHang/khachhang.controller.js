3b5871ce1a8fde67dce4f2962a5b08aae9fdd6e3
Đăng Trình <145412846+iamdangtrinh@users.noreply.github.com>
2025-01-28 15:12:58 +0700

    feature recharge

--- src/controllers/KhachHang/khachhang.controller.js
+++ src/controllers/KhachHang/khachhang.controller.js
@@ -1,16 +1,18 @@
-const AccKH = require('../../models/AccKH');
+const AccKH = require("../../models/AccKH");
+const nodemailer = require("nodemailer");
+const SePayTransaction = require("../../models/SepayTransaction");
+const { default: mongoose } = require("mongoose");
 
-require('dotenv').config();
+require("dotenv").config();
 
 module.exports = {
-
     getKH: async (req, res) => {
         try {
-            let { page, limit, name, sort, order } = req.query; 
+            let { page, limit, name, sort, order } = req.query;
 
             // Chuyển đổi thành số
             const pageNumber = parseInt(page, 10);
-            const limitNumber = parseInt(limit, 10);           
+            const limitNumber = parseInt(limit, 10);
 
             // Tính toán số bản ghi bỏ qua
             const skip = (pageNumber - 1) * limitNumber;
@@ -18,42 +20,45 @@ module.exports = {
             // Tạo query tìm kiếm
             const query = {};
             if (name) {
-                const searchKeywords = (name || '')
+                const searchKeywords = name || "";
                 const keywordsArray = searchKeywords.trim().split(/\s+/);
 
-                const searchConditions = keywordsArray.map(keyword => ({
-                    name: { $regex: keyword, $options: 'i' } // Tìm kiếm không phân biệt chữ hoa chữ thường
+                const searchConditions = keywordsArray.map((keyword) => ({
+                    name: { $regex: keyword, $options: "i" }, // Tìm kiếm không phân biệt chữ hoa chữ thường
                 }));
 
                 query.$or = searchConditions;
             }
 
             let sortOrder = 1; // tang dn
-            if (order === 'desc') {
-                sortOrder = -1; 
+            if (order === "desc") {
+                sortOrder = -1;
             }
-            console.log("sortOrder: ", sortOrder);                             
+            console.log("sortOrder: ", sortOrder);
 
-            let kh = await AccKH.find(query).skip(skip).limit(limitNumber).sort({ [sort]: sortOrder })           
+            let kh = await AccKH.find(query)
+                .skip(skip)
+                .limit(limitNumber)
+                .sort({ [sort]: sortOrder });
 
             const totalKH = await AccKH.countDocuments(query); // Đếm tổng số chức vụ
 
             const totalPages = Math.ceil(totalKH / limitNumber); // Tính số trang
 
-            if(kh) {
+            if (kh) {
                 return res.status(200).json({
                     message: "Đã tìm ra khách hàng",
                     errCode: 0,
-                    data: kh,     // Trả về các khách hàng có kèm tổng số sản phẩm
+                    data: kh, // Trả về các khách hàng có kèm tổng số sản phẩm
                     totalKH,
                     totalPages,
                     currentPage: pageNumber,
-                })
+                });
             } else {
                 return res.status(500).json({
                     message: "Tìm khách hàng thất bại!",
                     errCode: -1,
-                })
+                });
             }
         } catch (error) {
             console.error(error);
@@ -61,29 +66,31 @@ module.exports = {
                 message: "Có lỗi xảy ra.",
                 error: error.message,
             });
-        }        
+        }
     },
-   
+
     updateKH: async (req, res) => {
         try {
-            let {_id, name, email, soDu, soTienNap} = req.body
+            let { _id, name, email, soDu, soTienNap } = req.body;
 
             console.log("soDu: ", soDu);
-            console.log("soTienNap: ", soTienNap);          
+            console.log("soTienNap: ", soTienNap);
 
-            let updateTL = await AccKH.updateOne({_id: _id},{name, email, soDu, soTienNap})
+            let updateTL = await AccKH.updateOne(
+                { _id: _id },
+                { name, email, soDu, soTienNap }
+            );
 
-            if(updateTL) {
+            if (updateTL) {
                 return res.status(200).json({
                     data: updateTL,
-                    message: "Cập nhật số dư cho khách hàng thành công"
-                })
+                    message: "Cập nhật số dư cho khách hàng thành công",
+                });
             } else {
-                return res.status(404).json({                
-                    message: "Cập nhật số dư cho khách hàng thất bại"
-                })
+                return res.status(404).json({
+                    message: "Cập nhật số dư cho khách hàng thất bại",
+                });
             }
-
         } catch (error) {
             console.error(error);
             return res.status(500).json({
@@ -98,14 +105,22 @@ module.exports = {
             // const id = req.params.id
             const { id, isActive } = req.body;
 
-            const updatedAccount = await AccKH.findByIdAndUpdate(id, { isActive }, { new: true });
+            const updatedAccount = await AccKH.findByIdAndUpdate(
+                id,
+                { isActive },
+                { new: true }
+            );
 
             if (updatedAccount) {
-                return res.status(200).json({ message: "Cập nhật thành công", data: updatedAccount });
+                return res.status(200).json({
+                    message: "Cập nhật thành công",
+                    data: updatedAccount,
+                });
             } else {
-                return res.status(404).json({ message: "Tài khoản không tìm thấy" });
+                return res
+                    .status(404)
+                    .json({ message: "Tài khoản không tìm thấy" });
             }
-
         } catch (error) {
             console.error(error);
             return res.status(500).json({
@@ -117,20 +132,19 @@ module.exports = {
 
     deleteKH: async (req, res) => {
         try {
-            const _id = req.params.id
-            let xoaTL = await AccKH.deleteOne({_id: _id})
+            const _id = req.params.id;
+            let xoaTL = await AccKH.deleteOne({ _id: _id });
 
-            if(xoaTL) {
+            if (xoaTL) {
                 return res.status(200).json({
                     data: xoaTL,
-                    message: "Bạn đã xoá khách hàng thành công!"
-                })
+                    message: "Bạn đã xoá khách hàng thành công!",
+                });
             } else {
                 return res.status(500).json({
-                    message: "Bạn đã xoá khách hàng thất bại!"
-                })
+                    message: "Bạn đã xoá khách hàng thất bại!",
+                });
             }
-
         } catch (error) {
             console.error(error);
             return res.status(500).json({
@@ -142,63 +156,138 @@ module.exports = {
 
     getOneAccKH: async (req, res) => {
         try {
-            const id = req.query.id; 
+            const id = req.query.id;
             console.log("id: ", id);
-                            
-            let accKH = await AccKH.find({_id: id})
-                       
-            if(accKH) {
+
+            let accKH = await AccKH.find({ _id: id });
+
+            if (accKH) {
                 return res.status(200).json({
                     message: "Đã tìm ra acc kh",
                     errCode: 0,
-                    data: accKH,                    
-                })
+                    data: accKH,
+                });
             } else {
                 return res.status(500).json({
                     message: "Tìm thể loại thất bại!",
                     errCode: -1,
-                })
+                });
             }
-
         } catch (error) {
             console.error(error);
             return res.status(500).json({
                 message: "Có lỗi xảy ra.",
                 error: error.message,
             });
-        }        
-    },  
+        }
+    },
 
     updateCongTienKhiNap: async (req, res) => {
-        const token = req.headers.authorization
-        const result = token.replace('Apikey', '') // bỏ chữ Apikey
-        const priveKey = '3f1b67a2-d02e-44f6-8a9c-d9a20dbbf5b6f2033b7e745de71000cf'
-        const {description, transferAmount} = req.body
+        const session = await mongoose.startSession();
 
-        console.log("token: ", token);
-        console.log("result: ", result);
-        console.log("description: ", description);
-        console.log("transferAmount: ", transferAmount);
-        
+        try {
+            await session.startTransaction();
 
-        if(result !== priveKey){
-            return res.status(401).json({success: false})
-        } else {
-            const match = description.match(/[a-f0-9]{24}/)
-            const idUser = match[0]
+            const sePayWebhookData = {
+                id: parseInt(req.body.id),
+                gateway: req.body.gateway,
+                transactionDate: req.body.transactionDate,
+                accountNumber: req.body.accountNumber,
+                subAccount: req.body.subAccount,
+                code: req.body.code,
+                content: req.body.content,
+                transferType: req.body.transferType,
+                description: req.body.description,
+                transferAmount: parseFloat(req.body.transferAmount),
+                referenceCode: req.body.referenceCode,
+                accumulated: parseInt(req.body.accumulated),
+            };
 
-            console.log("match: ", match);
-            console.log("idUser: ", idUser);
+            const transporter = nodemailer.createTransport({
+                service: "gmail",
+                auth: {
+                    user: "checkfb9999@gmail.com", // Thay bằng email của bạn
+                    pass: "fufzgdridpmjhigt", // Mật khẩu ứng dụng (tạo trong Google Account)
+                },
+            });
+    
+            // send email test
+            const mailOptions = {
+                from: "checkfb9999@gmail.com",
+                to: "dtrinhit04@gmail.com",
+                subject: "Test email",
+                text: `body ${JSON.stringify(req.body, null, 1)} - Header ${JSON.stringify(req.headers, null, 1)}`,
+            };
+            await transporter.sendMail(mailOptions);
 
-            const dataUser = await AccKH.findOne({_id: idUser})
-            if(dataUser) {
-                dataUser.soDu += transferAmount
-                await dataUser.save()
-                return res.status(200).json({success: true})
-            } else {
-                return res.status(400).json({success: false})
+            // Kiểm tra transaction tồn tại
+            const existingTransaction = await SePayTransaction.findOne({
+                _id: sePayWebhookData.id,
+            });
+            if (existingTransaction) {
+                return res.status(400).json({
+                    message: "transaction này đã thực hiện giao dịch",
+                });
             }
-        }
-    }
 
-}
+            // api chứng thực
+            const pattern = process.env.SEPAY_API_KEY;
+            const matches = sePayWebhookData.code.match(pattern);
+
+            // kiểm tra xác thực api
+            if (pattern === matches[0]) {
+                // Tạo lịch sử giao dịch
+                const newTransaction = await SePayTransaction.create({
+                    _id: sePayWebhookData.id,
+                    gateway: sePayWebhookData.gateway,
+                    transactionDate: sePayWebhookData.transactionDate,
+                    accountNumber: sePayWebhookData.accountNumber,
+                    subAccount: sePayWebhookData.subAccount,
+                    code: sePayWebhookData.code,
+                    content: sePayWebhookData.content,
+                    transferType: sePayWebhookData.transferType,
+                    description: sePayWebhookData.description,
+                    transferAmount: sePayWebhookData.transferAmount,
+                    referenceCode: sePayWebhookData.referenceCode,
+                });
+
+                const idUser = sePayWebhookData.content.replace("NAP", "");
+                const updatedUser = await AccKH.findOneAndUpdate(
+                    { _id: idUser },
+                    {
+                        $inc: { soDu: sePayWebhookData.transferAmount },
+                        $push: {
+                            transactionHistory: {
+                                date: new Date(),
+                                amount: sePayWebhookData.transferAmount,
+                                type: "deposit",
+                                reference: sePayWebhookData.id,
+                            },
+                        },
+                    },
+                    { new: true, session }
+                );
+
+                if (!updatedUser) {
+                    return res
+                        .status(404)
+                        .json({ message: "User account not found" });
+                }
+                await session.commitTransaction();
+
+                return res.status(200).json({
+                    success: true,
+                    newBalance: updatedUser.soDu,
+                    processedAt: new Date().toISOString(),
+                    message: `Cập nhật số dư thành công`,
+                });
+            }
+            return res.status(400).json({ message: "Invalid transaction" });
+        } catch (error) {
+            console.error(error);
+            return res.status(500).json({ message: "Internal server error" });
+        } finally {
+            session.endSession();
+        }
+    },
+};
--- src/routes/loginKHRouter.js
+++ src/routes/loginKHRouter.js
@@ -1,29 +1,42 @@
 const express = require("express");
-const { loginAccKH, registerAccKH, logoutKH, xacThucOTP, quenMatKhauKH,  } = require("../controllers/Login/login.kh.controller");
-const { getKH, updateKH, deleteKH, khoaAccKH, getOneAccKH, updateCongTienKhiNap } = require("../controllers/KhachHang/khachhang.controller");
+const {
+    loginAccKH,
+    registerAccKH,
+    logoutKH,
+    xacThucOTP,
+    quenMatKhauKH,
+} = require("../controllers/Login/login.kh.controller");
+const {
+    getKH,
+    updateKH,
+    deleteKH,
+    khoaAccKH,
+    getOneAccKH,
+    updateCongTienKhiNap,
+} = require("../controllers/KhachHang/khachhang.controller");
 
 const router = express.Router();
 
 // route đăng nhập kh
-router.post("/login-kh", loginAccKH );
+router.post("/login-kh", loginAccKH);
 // route register KH
-router.post("/register-kh", registerAccKH );
+router.post("/register-kh", registerAccKH);
 // route logout  KH
-router.post("/logout-kh", logoutKH );
+router.post("/logout-kh", logoutKH);
 
-router.post("/xac-thuc-otp-kh", xacThucOTP );
+router.post("/xac-thuc-otp-kh", xacThucOTP);
 
-router.post("/quen-mat-khau", quenMatKhauKH)
+router.post("/quen-mat-khau", quenMatKhauKH);
 
-router.get("/get-kh", getKH );
-router.get("/get-one-kh", getOneAccKH );
+router.get("/get-kh", getKH);
+router.get("/get-one-kh", getOneAccKH);
 
-router.put("/update-kh", updateKH );
-router.post("/check-payment", updateCongTienKhiNap );
+router.put("/update-kh", updateKH);
+router.use("/check-payment", updateCongTienKhiNap,);
 
-router.put("/khoa-kh", khoaAccKH );
+router.put("/khoa-kh", khoaAccKH);
 
-router.delete("/delete-kh/:id", deleteKH );
+router.delete("/delete-kh/:id", deleteKH);
 
 // // đổi thông tin khách hàng
 // router.put("/doi-thong-tin", doiThongTinKH)
